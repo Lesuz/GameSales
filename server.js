37a7ecd55dd2f744ecbb74a26fc53b4d54167dea
Lesuz <50194165+Lesuz@users.noreply.github.com>
2021-03-24 17:37:53 +0200

    ajax call and styling

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+const request = require("request");
 var express = require('express');
 var app = express();
+var data;
+
+const PORT = process.env.PORT || 3000;
+
+request (
+    "https://www.cheapshark.com/api/1.0/deals",
+    {json: true},
+    (err, res, body) => {
+        if(err) {
+            return console.log(err)
+        }
+        body.sort(function(a,b) {
+            if(a.title < b.title) { return -1;}
+            if(a.title > b.title) { return 1;}
+        })
+        data = body;
+        console.log(data);
+    }
+);
 
 app.set('view engine', 'ejs');
 
@@ -8,8 +28,7 @@ app.get('/', function(req, res) {
 })
 
 app.get('/steamsales', function(req, res) {
-    res.render('pages/steamsales')
+    res.render('pages/steamsales', {data:data})
 })
 
-app.listen(8081);
-console.log("Hi");
+app.listen(PORT);

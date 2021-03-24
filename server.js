7bca07c65d9c88809a4b82d6388a97736346c2f1
Lesuz <50194165+Lesuz@users.noreply.github.com>
2021-03-24 18:26:49 +0200

    small fixes, clean up code, added more comments

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express();
 var data;
 
+// Environment chooses which port is being used, or using port 3000 as a localhost
 const PORT = process.env.PORT || 3000;
 
+// API call
 request (
     "https://www.cheapshark.com/api/1.0/deals",
     {json: true},
@@ -12,12 +14,8 @@ request (
         if(err) {
             return console.log(err)
         }
-        body.sort(function(a,b) {
-            if(a.title < b.title) { return -1;}
-            if(a.title > b.title) { return 1;}
-        })
+        // saving sorted array into new variable
         data = body;
-        console.log(data);
     }
 );
 
@@ -27,7 +25,15 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 })
 
+// rendering steamsales page and giving it the gotten data from the API
 app.get('/steamsales', function(req, res) {
+
+    // sorting gotten data in an alphabetcal order
+    data.sort(function(a,b) {
+        if(a.title < b.title) { return -1;}
+        if(a.title > b.title) { return 1;}
+    })
+    
     res.render('pages/steamsales', {data:data})
 })
 
